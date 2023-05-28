58c10a9ef5765b7a8942fdbac5efd18142beb946
David R. Myers <davidrmyersii@gmail.com>
2023-05-27 21:20:06 -0400

    Update chat action to use new concepts

--- src/actions/chat.ts
+++ src/actions/chat.ts
@@ -1,65 +1,42 @@
-import { writeFileSync } from 'node:fs'
-import { join } from 'node:path'
 import chalk from 'chalk'
 import { env } from '../../env'
-import { type ApiChatMessage, buildApiInstance } from '../api'
 import { type Peripherals } from '../peripherals'
-import { root } from '../filesystem'
+import { type Message, messages } from '#data/models'
+import { openaiIntegration } from '#integrations'
+import { adaptChat, openaiAdapter } from '#models/chat'
 
-export const assistant = (content: string, name?: string) => {
-  return {
-    content,
-    name,
-    role: 'assistant',
-  } satisfies ApiChatMessage
-}
-
-export const human = (content: string, name?: string) => {
-  return {
-    content,
-    name,
-    role: 'user',
-  } satisfies ApiChatMessage
-}
-
-export const system = (content: string, name?: string) => {
-  return {
-    content,
-    name,
-    role: 'system',
-  } satisfies ApiChatMessage
-}
-
-export const chat = async ({ io }: Peripherals) => {
-  const model = 'gpt-3.5-turbo'
+export const chat = async (peripherals: Peripherals) => {
+  // Todo: Add support for overriding the model.
+  // const model = 'gpt-3.5-turbo'
   // const model = 'gpt-4'
+
   const {
     apiKey,
     // organization,
-    userId,
   } = env()
-  const api = buildApiInstance({
-    apiKey,
-    // organization,
-    userId,
-  })
-  const messages = <ApiChatMessage[]>[]
+
+  const chatModel = adaptChat(
+    openaiAdapter(
+      openaiIntegration({
+        apiKey,
+        // organization,
+      }, peripherals),
+    ),
+  )
+
+  const chatMessages = <Message[]>[]
 
   while (true) {
-    const humanAnswer = await io.prompt(chalk.green('You:\n'))
-    const humanMessage = human(humanAnswer)
-    const { json: completion } = await api.chat({ model, messages: [...messages, humanMessage] })
+    const humanAnswer = await peripherals.io.prompt(chalk.green('You:\n'))
+    const humanMessage = messages.human({ text: humanAnswer })
+    const assistantMessage = await chatModel.send([...chatMessages, humanMessage])
 
-    // Messages are only added if the API call is successful.
-    messages.push(humanMessage)
-    writeFileSync(join(root, 'output', `chat-${Date.now()}.json`), JSON.stringify(completion, null, 2))
-
-    const { message: assistantMessage } = completion.choices[0]
+    chatMessages.push(humanMessage)
 
     if (assistantMessage) {
-      messages.push(assistantMessage)
+      chatMessages.push(assistantMessage)
 
-      await io.write(`\n${chalk.cyan('Agent:')}\n${assistantMessage.content}\n\n`)
+      await peripherals.io.write(`\n${chalk.cyan('Agent:')}\n${assistantMessage.text}\n\n`)
     }
   }
 }
