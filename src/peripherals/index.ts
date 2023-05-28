2ac73f6a46b340ba469b9eb65f9b0d01d995f639
David R. Myers <davidrmyersii@gmail.com>
2023-05-27 21:18:23 -0400

    Add storage to peripherals util

--- src/peripherals/index.ts
+++ src/peripherals/index.ts
@@ -1,19 +1,23 @@
 import { type HttpAdapter, type HttpPeripheral, adaptHttp } from './http'
 import { type IoAdapter, type IoPeripheral, adaptIo } from './io'
+import { type StorageAdapter, type StoragePeripheral, adaptStorage } from './storage'
 
 export type PeripheralAdapters = {
   http: HttpAdapter,
   io: IoAdapter,
+  storage: StorageAdapter,
 }
 
 export type Peripherals = {
   http: HttpPeripheral,
   io: IoPeripheral,
+  storage: StoragePeripheral,
 }
 
-export const adapt = ({ http, io }: Partial<PeripheralAdapters>) => {
+export const adapt = ({ http, io, storage }: Partial<PeripheralAdapters>) => {
   return {
     http: adaptHttp(http),
     io: adaptIo(io),
+    storage: adaptStorage(storage),
   } satisfies Peripherals
 }
