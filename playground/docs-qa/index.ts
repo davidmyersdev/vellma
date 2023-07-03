d8ada04297d1f31dbfc547207ed1e61ae1703b06
David R. Myers <davidrmyersii@gmail.com>
2023-07-03 17:50:56 -0400

    Add an example for Q&A over local files

---
+++ playground/docs-qa/index.ts
@@ -0,0 +1,61 @@
+import { readFileSync } from 'node:fs'
+import { dirname, join, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import chalk from 'chalk'
+import { type Vector } from 'ellma'
+import { openai } from 'ellma/integrations'
+import { useChat, useEmbedding } from 'ellma/models'
+import { ioTerminal, storageFile, useIo, useStorage } from 'ellma/peripherals'
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { similarity } from 'ml-distance'
+
+const root = dirname(fileURLToPath(import.meta.url))
+
+// Peripheral configuration
+const dbFile = resolve(join('tmp', 'playground-embedding-qa.json'))
+const io = useIo(ioTerminal())
+const storage = useStorage(storageFile(dbFile))
+const peripherals = { io, storage }
+
+// Ellma initialization
+const integration = openai({ apiKey: import.meta.env.VITE_OPENAI_API_KEY, peripherals })
+const { model: embeddingModel } = useEmbedding({ integration, peripherals })
+
+// Input configuration
+const file = readFileSync(join(root, 'at-s2e24.txt'), 'utf-8')
+const splitter = new RecursiveCharacterTextSplitter()
+const textChunks = await splitter.splitText(file)
+
+const vectors = [] as Vector[]
+
+// Create vectors for all text chunks
+for (const text of textChunks) {
+  const vector = await embeddingModel.generate(text)
+
+  vectors.push(vector)
+}
+
+// Q&A loop
+while (true) {
+  // Turn the question into a vector.
+  const question = await io.prompt(chalk.green('Question:\n'))
+  const questionVector = await embeddingModel.generate(question)
+  const vectorWithSimilarity = (vector: Vector) => {
+    return {
+      ...vector,
+      similarity: similarity.cosine(vector.embedding, questionVector.embedding),
+    }
+  }
+
+  // Find the relevant vectors based on the question.
+  const relevantChunks = vectors.map(vectorWithSimilarity).sort((a, b) => b.similarity - a.similarity).slice(0, 5)
+  const relevantText = relevantChunks.map(({ embeddingSource }) => embeddingSource).join('\n\n')
+
+  // Send the relevant chunks of text along with the question to the chat model.
+  const { factory: chatFactory, model: chatModel } = useChat({ integration, peripherals })
+  const systemPrompt = chatFactory.system({ text: `You are an assistant that answers questions about the following information:\n\n${relevantText}` })
+  const humanQuestion = chatFactory.human({ text: question })
+  const answer = await chatModel.generate(systemPrompt, humanQuestion)
+
+  await io.write(`\n${chalk.cyan('Answer:')}\n${answer.text}\n\n`)
+}
