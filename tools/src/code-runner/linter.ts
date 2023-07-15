2f9f072f353176bd7ae51be53868ce5c8f9e0c3d
David R. Myers <davidrmyersii@gmail.com>
2023-07-15 18:21:34 -0400

    Update code-runner linter config

--- tools/src/code-runner/linter.ts
+++ tools/src/code-runner/linter.ts
@@ -1,7 +1,27 @@
-import { ESLint } from 'eslint'
+import { ESLint, type Linter } from 'eslint'
 
-export const format = async (code: string) => {
-  const eslint = new ESLint({ fix: true })
+const defaultOverrides = (): Linter.Config => {
+  return {
+    rules: {
+      'brace-style': [
+        'error',
+        '1tbs',
+      ],
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: '*', next: ['block', 'block-like', 'for', 'function'] },
+        { blankLine: 'always', prev: ['block', 'block-like', 'for', 'function'], next: '*' },
+      ],
+    },
+  }
+}
+
+export const format = async (code: string, overrides = defaultOverrides()) => {
+  const eslint = new ESLint({
+    fix: true,
+    overrideConfig: overrides,
+  })
+
   const lintResults = await eslint.lintText(code)
   const formattedCode = lintResults.map(lintResult => lintResult.output).join('\n').trim()
 
