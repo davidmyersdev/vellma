42f629ac7025c771b45caf1a65625bc9fb8e4f31
David R. Myers <davidrmyersii@gmail.com>
2023-08-02 15:59:07 -0500

    Add basic chat model test

---
+++ test/unit/models/src/chat/index.test.ts
@@ -0,0 +1,26 @@
+import { openai } from 'vellma/integrations'
+import { useChat } from 'vellma/models'
+import { useHttp, useIo, useLogger } from 'vellma/peripherals'
+import { describe, expect, it } from 'vitest'
+import { mockHttp, mockIo, mockLogger } from '#test/utils'
+
+describe('useChat', () => {
+  const mockedHttp = mockHttp()
+  const http = useHttp(mockedHttp)
+  const mockedIo = mockIo()
+  const io = useIo(mockedIo)
+  const mockedLogger = mockLogger()
+  const logger = useLogger(mockedLogger)
+  const peripherals = { http, io, logger }
+
+  it('sends messages to the underlying integration', async () => {
+    const integration = openai({ apiKey: 'test', peripherals })
+    const { factory, model } = useChat({ integration })
+
+    await model.add(factory.system({ text: 'You are a helpful assistant.' }))
+
+    const reply = await model.generate(factory.human({ text: 'Hi. I need some help.' }))
+
+    expect(reply.text).toEqual('Hi there. How can I help you?')
+  })
+})
